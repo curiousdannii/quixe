70f66f6eb733e6daf9cd17f05e5f5fdb38dea56f
Dannii Willis <curiousdannii@gmail.com>
2016-10-22 22:41:21 +1000

    Testing code for ZVM - use play-remote-full.html

--- src/quixe/gi_load.js
+++ src/quixe/gi_load.js
@@ -128,8 +128,9 @@ function load_run(optobj, image, image_format) {
     /* Set the default entries for the interface objects that come from
        other libraries. (If no such libraries have been loaded, then
        these do nothing, but game_options can still supply these entries.) */
-    all_options.vm = window.Quixe;
+    all_options.vm = window.engine = new window.ZVM();
     all_options.io = window.Glk;
+	all_options.glk = window.Glk;
     
     if (!optobj)
         optobj = window.game_options;
@@ -618,7 +619,7 @@ function unpack_blorb(image) {
         el.len = chunklen;
         el.content = null;
 
-        if (el.usage == "Exec" && el.num == 0 && chunktype == "GLUL") {
+        if (el.usage == "Exec" && el.num == 0 && ( chunktype == "GLUL" ||chunktype == "ZCOD" )) {
             result = image.slice(pos, pos+chunklen);
         }
         else {
